1aac64d2498c720583c1259ff609c88288ce6097
Đình Duy Võ <duy7210@gmail.com>
2023-04-04 17:04:42 +0700

    init successful- added react-chat-engine

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {ChatEngine} from 'react-chat-engine'
+
+const App = () => {
+	return (
+		<ChatEngine
+			projectID="82733920-a71b-484c-b58e-694dfa430d1a"
+			userName="root"
+			userSecret="00000"
+		/>
+	)
+}
+
+//export default App;
+export default App
